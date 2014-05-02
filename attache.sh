2655fde52d0381f54a95f4ad2992ad140e766be4
Geoff Shannon <geoffpshannon@gmail.com>
2014-05-02 13:41:57 -0700

    Make import add the new file into to git

--- attache.sh
+++ attache.sh
@@ -13,6 +13,25 @@ PROGNAME=$(basename $0)
 
 DEFAULT_ATTACHE_DIR=~/.home
 
+attache_add_file() {
+    TEMP=$(getopt -o 'h' -l 'help' -n "$PROGNAME $subcommand" -- "$@")
+
+    if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+
+    eval set -- "$TEMP"
+
+    while true; do
+        case "$1" in
+            -- ) shift; break ;;
+        esac
+    done
+
+    ATTACHE_FILE=${1#$DEFAULT_ATTACHE_DIR}
+    [ -f $ATTACHE_FILE ] || echo "File $1 does not exist in $DEFAULT_ATTACHE_DIR" ; exit 1
+
+    git add -vf $ATTACHE_FILE
+}
+
 sub_help() {
     echo "Usage: $PROGNAME <subcommand> [options]\n"
     echo "Subcommands:"
@@ -55,6 +74,7 @@ sub_import() {
         mkdir -vp $(basename $NEWFILE)
         mv -vn $OLDFILE $NEWFILE
         ln -vs $NEWFILE $OLDFILE
+        attache_add_file $NEWFILE
 
     elif [ -d $0 ]; then
         echo "Nothing yet"
