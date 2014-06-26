a5bb48c1e6ed308779192dfc14f43cd415eade4b
Geoff Shannon <geoffpshannon@gmail.com>
2014-06-25 21:48:44 -0700

    Fix runtests.sh to try path urchin and /tmp/urchin

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+URCHIN=$(which urchin)
+URCHIN=${URCHIN:-/tmp/urchin}
+
 export TESTDIR=".test-place"
 
 printf '\n\033[0;33m%s\033[0m\n' "Running tests in test"
-/tmp/urchin test/
+$URCHIN test/
