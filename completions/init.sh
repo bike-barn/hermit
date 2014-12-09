995cd0a2a4f8f8b0d5af725ded49b0b36f2f74a0
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-09 17:53:39 -0500

    Add file for loading completions correctly in bash and zsh

---
+++ completions/init.sh
@@ -0,0 +1,10 @@
+# init.sh
+
+if [ -n "$BASH_VERSION" ]; then
+    root="$(dirname "${BASH_SOURCE[0]}")"
+    source "$root/completion.bash"
+
+elif [ -n "$ZSH_VERSION" ]; then
+    root="$(dirname "$0")"
+    source "$root/completion.zsh"
+fi
