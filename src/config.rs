e8b941b99780f4522d73734d27643bf0000dbe4b
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-03 23:19:41 -0600

    Use to_owned instead of to_string
    For conversion from &str to String.  It's apparently much leaner.

--- src/config.rs
+++ src/config.rs
@@ -88,12 +88,12 @@ pub mod mock {
         }
 
         fn set_current_shell_name(&mut self, name: &str) -> io::Result<()> {
-            self.current_shell = name.to_string();
+            self.current_shell = name.to_owned();
             Ok(())
         }
 
         fn does_shell_exist(&self, name: &str) -> bool {
-            self.allowed_shell_names.contains(&name.to_string())
+            self.allowed_shell_names.contains(&name.to_owned())
         }
     }
 }
