eabb038daa5732fb7e9d0cf1617b70dd8c2cc342
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-10 14:23:14 -0600

    Add an init_shell method to Hermit struct
    Will be used by the init subcommand.

--- src/hermit.rs
+++ src/hermit.rs
@@ -1,10 +1,7 @@
 use std::io;
 use config::Config;
+use file_operations::FileOperations;
 use shell::Shell;
-
-struct Hermit<T: Config> {
-    config: T,
-}
 
 #[derive(Copy,Clone)]
 #[derive(PartialEq,Eq)]
@@ -19,17 +16,26 @@ impl From<io::Error> for Error {
     }
 }
 
+pub struct Hermit<T: Config> {
+    config: T,
+}
+
 impl<T: Config> Hermit<T> {
-    fn current_shell(&self) -> Shell {
+    pub fn current_shell(&self) -> Shell {
         Shell::new(self.config.current_shell_name(), self.config.root_path())
     }
 
-    fn set_current_shell(&mut self, name: &str) -> Result<(), Error> {
+    pub fn set_current_shell(&mut self, name: &str) -> Result<(), Error> {
         if self.config.does_shell_exist(name) {
             self.config.set_current_shell_name(name).map_err(From::from)
         } else {
             Err(Error::ShellDoesNotExist)
         }
+    }
+
+    pub fn init_shell(&self, file_ops: &mut FileOperations, name: &str) {
+        let path = self.config.shell_root_path().join(name);
+        file_ops.create_git_repo(path);
     }
 }
 
@@ -39,6 +45,8 @@ mod tests {
 
     use config::Config;
     use config::mock::MockConfig;
+    use file_operations::FileOperations;
+    use file_operations::Op;
 
     use super::{Error, Hermit};
 
@@ -84,5 +92,21 @@ mod tests {
         let res = hermit.set_current_shell("non-existent");
         assert!(res.is_err());
         assert_eq!(res.err().unwrap(), Error::ShellDoesNotExist);
+    }
+
+    #[test]
+    fn can_initialize_a_new_shell() {
+        let config = MockConfig {
+            root_path: PathBuf::from(".hermit-config"),
+            allowed_shell_names: vec!["default".to_owned()],
+            current_shell: "default".to_owned(),
+        };
+        let hermit = hermit(&config);
+        let mut file_ops = FileOperations::rooted_at("/home/geoff");
+
+        hermit.init_shell(&mut file_ops, "new-one");
+        let first_op = &file_ops.operations[0];
+        assert_eq!(*first_op,
+                   Op::GitInit(PathBuf::from("/home/geoff/.hermit-config/shells/new-one")));
     }
 }
