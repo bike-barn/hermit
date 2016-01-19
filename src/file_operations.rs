0385f7af857df569f5458535879e24f7a7622a77
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-18 19:42:05 -0600

    Init git dirs with no_reinit
    This will give us an Err when we reinitialize so we can detect
    reinitializing a shell and show the user a message about it.

--- src/file_operations.rs
+++ src/file_operations.rs
@@ -59,6 +59,7 @@ type Result = result::Result<(), Error>;
 pub struct FileOperations {
     pub root: PathBuf,
     pub operations: Vec<Op>,
+    git_init_opts: git2::RepositoryInitOptions,
 }
 
 impl FileOperations {
@@ -66,7 +67,15 @@ impl FileOperations {
         FileOperations {
             root: PathBuf::from(path.as_ref()),
             operations: vec![],
+            git_init_opts: FileOperations::default_git_opts(),
         }
+    }
+
+    fn default_git_opts() -> git2::RepositoryInitOptions {
+        let mut opts = git2::RepositoryInitOptions::new();
+        opts.no_reinit(true);
+
+        opts
     }
 
     pub fn create_dir<P: AsRef<Path>>(&mut self, name: P) {
@@ -97,9 +106,13 @@ impl FileOperations {
         match op {
             Op::MkDir(dir) => try!(fs::create_dir(dir)),
             Op::MkDirAll(dir) => try!(fs::create_dir_all(dir)),
-            Op::GitInit(dir) => try!(git2::Repository::init(dir).map(|_| ())),
+            Op::GitInit(dir) => try!(self.git_init(dir)),
         };
         Ok(())
+    }
+
+    fn git_init(&self, dir: PathBuf) -> result::Result<(), git2::Error> {
+        git2::Repository::init_opts(dir, &self.git_init_opts).map(|_| ())
     }
 }
 
@@ -191,5 +204,19 @@ mod tests {
         assert!(test_root.join(&path).join(".git").is_dir());
 
         clean_up(&test_root);
+    }
+
+    #[test]
+    fn wont_re_init_an_already_existing_repository() {
+        let test_root = set_up("git-re-init");
+        let mut file_set = FileOperations::rooted_at(&test_root);
+
+        file_set.create_git_repo(".");
+        file_set.create_git_repo(".");
+
+        let results = file_set.commit();
+        assert_eq!(results.len(), 2);
+        assert!(results[0].is_ok());
+        assert!(results[1].is_err());
     }
 }
