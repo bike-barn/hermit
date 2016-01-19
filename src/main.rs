558a27b90da0e984bdece424a009902d0d561665
Geoff Shannon <geoffpshannon@gmail.com>
2016-01-19 00:12:42 -0600

    Macroify configure_* function definitions.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ mod hermit;
 mod shell;
 mod file_operations;
 
+#[macro_use]
+mod macros;
+
 use std::env;
 use std::error::Error;
 use std::path::PathBuf;
@@ -21,81 +24,6 @@ use file_operations::FileOperations;
 
 #[cfg(test)]
 mod test;
-
-fn make_app_config<'a, 'b, 'c, 'd, 'e, 'f>() -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let version = env!("CARGO_PKG_VERSION");
-
-    let app = App::new("hermit")
-                  .version(version)
-                  .author("Bike Barn <https://github.com/bike-barn/hermit>")
-                  .about("A home directory configuration management assistant.")
-                  .setting(AppSettings::SubcommandRequiredElseHelp)
-                  .setting(AppSettings::VersionlessSubcommands);
-
-    let app = configure_add(app);
-    let app = configure_clone(app);
-    let app = configure_doctor(app);
-    let app = configure_git(app);
-    let app = configure_init(app);
-    let app = configure_nuke(app);
-    let app = configure_status(app);
-    let app = configure_use(app);
-
-    app
-}
-
-fn configure_add<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                         -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("add").about("Add files to your hermit shell");
-    app.subcommand(subcommand)
-}
-
-fn configure_clone<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                           -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("clone")
-                         .about("Create a local shell from an existing remote shell");
-    app.subcommand(subcommand)
-}
-
-fn configure_doctor<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                            -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("").about("Make sure your hermit setup is sane");
-    app.subcommand(subcommand)
-}
-
-fn configure_git<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                         -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("").about("Run git operations on the current shell");
-    app.subcommand(subcommand)
-}
-
-fn configure_init<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                          -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("")
-                         .about("Create a new hermit shell called SHELL_NAME. If no shell name \
-                                 is given, \"default\" is used.")
-                         .arg(Arg::with_name("SHELL_NAME")
-                                  .help("The name of the shell to be created."));
-    app.subcommand(subcommand)
-}
-
-fn configure_nuke<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                          -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("").about("Permanently remove a hermit shell");
-    app.subcommand(subcommand)
-}
-
-fn configure_status<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                            -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("").about("Display the status of your hermit shell");
-    app.subcommand(subcommand)
-}
-
-fn configure_use<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
-                                         -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-    let subcommand = SubCommand::with_name("").about("Switch to using a different hermit shell");
-    app.subcommand(subcommand)
-}
 
 fn main() {
     let app = make_app_config();
@@ -170,3 +98,60 @@ fn get_hermit_dir() -> Option<PathBuf> {
 fn default_hermit_dir() -> Option<PathBuf> {
     env::home_dir().map(|home| home.join(".config/hermit"))
 }
+
+fn make_app_config<'a, 'b, 'c, 'd, 'e, 'f>() -> App<'a, 'b, 'c, 'd, 'e, 'f> {
+    let version = env!("CARGO_PKG_VERSION");
+
+    let app = App::new("hermit")
+                  .version(version)
+                  .author("Bike Barn <https://github.com/bike-barn/hermit>")
+                  .about("A home directory configuration management assistant.")
+                  .setting(AppSettings::SubcommandRequiredElseHelp)
+                  .setting(AppSettings::VersionlessSubcommands);
+
+    let app = configure_add(app);
+    let app = configure_clone(app);
+    let app = configure_doctor(app);
+    let app = configure_git(app);
+    let app = configure_init(app);
+    let app = configure_nuke(app);
+    let app = configure_status(app);
+    let app = configure_use(app);
+
+    app
+}
+
+configure_subcommand!(add, configure_add {
+   about("Add files to your hermit shell")
+});
+
+configure_subcommand!(clone, configure_clone {
+    about("Create a local shell from an existing remote shell")
+});
+
+configure_subcommand!(doctor, configure_doctor {
+    about("Make sure your hermit setup is sane")
+});
+
+configure_subcommand!(git, configure_git {
+    about("Run git operations on the current shell")
+});
+
+configure_subcommand!(init, configure_init {
+    about("Create a new hermit shell called SHELL_NAME. If no shell name \
+           is given, \"default\" is used.");
+    arg(Arg::with_name("SHELL_NAME")
+        .help("The name of the shell to be created."))
+});
+
+configure_subcommand!(nuke, configure_nuke {
+    about("Permanently remove a hermit shell")
+});
+
+configure_subcommand!(status, configure_status {
+    about("Display the status of your hermit shell")
+});
+
+configure_subcommand!(use, configure_use {
+    about("Switch to using a different hermit shell")
+});
