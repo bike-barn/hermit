1f826fe50f359d6b3c17448b2cc57d87ce5850bf
Geoff Shannon <geoffpshannon@gmail.com>
2016-10-27 23:36:42 -0500

    Rework subcommand macro syntax
    Mostly playing around with what you can do with rust macros.  It's
    unfortunate that they need to have one matched delimiter pair
    surrounding the whole macro body. If that wasn't true I have a much
    nicer syntax in mind, but this is arguably better I think.

--- src/macros.rs
+++ src/macros.rs
@@ -1,14 +1,14 @@
 macro_rules! subcommand {
-    (
-        $name:ident, $fn_name:ident {
+    {
+        $fn_name:ident ( $name:expr ) {
             $(
-                $method:ident($($args:expr),*)
-            );*
+                $method:ident ( $($args:expr),* )
+            )*
         }
-    ) => {
+    } => {
         fn $fn_name<'a, 'b, 'c, 'd, 'e, 'f>(app: App<'a, 'b, 'c, 'd, 'e, 'f>)
                                                    -> App<'a, 'b, 'c, 'd, 'e, 'f> {
-            let subcommand = SubCommand::with_name(stringify!($name))
+            let subcommand = SubCommand::with_name($name)
                 $(
                     .$method($($args),*)
                 )*;
