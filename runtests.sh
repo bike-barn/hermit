4ae21bc6b8d00006764ad5d04c7c366130aea31c
Geoff Shannon <geoffpshannon@gmail.com>
2018-07-07 22:29:49 -0700

    Add a function for running the tests to runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+function run_tests() {
+    export TEST_PROFILE_NAME=$1
+    printf '\n\033[0;33m%s\033[0m\n' "Running tests in test with profile name $TEST_PROFILE_NAME"
+    $URCHIN test/
+}
+
 URCHIN=$(which urchin)
 URCHIN=${URCHIN:-/tmp/urchin}
 
@@ -11,13 +17,11 @@ cargo test  --verbose
 export TESTDIR=".test-place"
 
 # Integration test shell hermit
+
 export PROFILE_DIR_NAME=profiles
-export TEST_PROFILE_NAME=default
 
-printf '\n\033[0;33m%s\033[0m\n' "Running tests in test"
-$URCHIN test/
+RANDOM_PROFILE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
-export TEST_PROFILE_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-printf '\n\033[0;33m%s\033[0m\n' "Running tests in test with profile name $TEST_PROFILE_NAME"
+run_tests default
 
-$URCHIN test/
+run_tests ${RANDOM_PROFILE_NAME}
